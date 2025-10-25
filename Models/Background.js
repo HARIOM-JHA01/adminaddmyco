1cddfb22855d9d329e6758c3b42feedddb72f3bd
HARIOM-JHA01 <jhahariom040@gmail.com>
2025-10-25 15:40:14 +0530

    fix: update default value for backgroundcolor in Background model

--- Models/Background.js
+++ Models/Background.js
@@ -2,16 +2,16 @@ import mongoose from "mongoose";
 // import { setImageUrl, validatorError } from "../Common.js";
 
 const backgroundSchema = new mongoose.Schema({
-    user_id: { type: mongoose.Schema.Types.ObjectId, default: null, ref: "User" },
-    iconcolor:{type: String, default: null},
-    fontcolor:{type: String, default: null},
-    backgroundcolor:{type: String, default: "#FFFFFF"},
-    Thumbnail :{ type: String },
-    date: { type: Date, default: Date.now }
+  user_id: { type: mongoose.Schema.Types.ObjectId, default: null, ref: "User" },
+  iconcolor: { type: String, default: null },
+  fontcolor: { type: String, default: null },
+  backgroundcolor: { type: String, default: null },
+  Thumbnail: { type: String, default: null },
+  date: { type: Date, default: Date.now },
 });
 
-//  Compiling Schema                            
+//  Compiling Schema
 // backgroundSchema.set('toObject', { getters: true })
 // backgroundSchema.set('toJSON', { getters: true })
 const BackgroundModel = mongoose.model("Background", backgroundSchema);
-export default BackgroundModel
+export default BackgroundModel;
